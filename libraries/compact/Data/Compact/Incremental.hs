4b12ae1b5304505a8e217780aa8e1ebf1d3e8e4a
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-01-14 19:54:06 -0800

    CNF: first stab at incremental Compact allocation
    New primop, compactAppendOne, which does one level of evacing
    in the compact (plus any indirection adjusting if necessary),
    assuming all pointers are already inside it (and in a debug build
    it would complain loudly otherwise).
    New type class, Compactable, which does manual unfolding of objects
    and appends one by one using compactAppendOne (but also checks at
    each step if anything was already evaluated and is in a compact) -
    this alone avoids the repeated deepseq problem, but it is probably
    slower in that the calls are recursive instead of using the GC
    algorithm. And that's why it can be sidestepped by using an
    instance that calls the old compactAppend on a deepseq-ed object.
    New monad, CompactM, which runs the code so that all values are
    appended in the Compact as soon as they are generated.
    
    No new tests, yet.

---
+++ libraries/compact/Data/Compact/Incremental.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Compact
+-- Copyright   :  (c) The University of Glasgow 2001-2009
+--                (c) Giovanni Campagna <gcampagn@cs.stanford.edu> 2014
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  unstable
+-- Portability :  non-portable (GHC Extensions)
+--
+-- This module provides a data structure, called a Compact, for holding
+-- a set of fully evaluated Haskell values in a consecutive block of
+-- memory.
+--
+-- As the data fully evaluated and pure (thus immutable), it maintains
+-- the invariant that no memory reference exist from objects inside the
+-- Compact to objects outside, thus allowing very fast garbage collection
+-- (at the expense of increased memory usage, because the entire set of
+-- object is kept alive if any object is alive).
+--
+-- This is similar to Data.Compact, but it supports incremental evaluation
+-- into Compact structures.
+--
+-- /Since: 1.0.0/
+module Data.Compact.Incremental (
+  Compact,
+  compactGetRoot,
+  compactResize,
+
+  compactNew,
+  compactAppendOne,
+  compactAppendRecursively,
+  compactAppendEvaled,
+
+  Compactable,
+  defaultCompactNFData,
+  ) where
+
+-- Write down all GHC.Prim deps explicitly to keep them at minimum
+import GHC.Prim (Compact#,
+                 compactNew#,
+                 compactAppend#,
+                 compactAppendOne#,
+                 compactContains#,
+                 compactContainsAny#,
+                 Addr#,
+                 nullAddr#,
+                 eqAddr#,
+                 addrToAny#,
+                 State#,
+                 RealWorld,
+                 Int#,
+                 )
+-- We need to import Word from GHC.Types to see the representation
+-- and to able to access the Word# to pass down the primops
+import GHC.Types (IO(..), Word(..), isTrue#)
+
+import Data.Compact.Imp(Compact(..),
+                        compactGetRoot,
+                        compactGetBuffer,
+                        compactResize,
+                        compactAppendEvaledInternal,
+                        maybeMakeCompact)
+
+import Control.DeepSeq (NFData, force)
+
+-- not strictly required, but it makes code easier to read
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class
+
+compactNew :: Compactable a => Word -> a -> IO (Maybe (Compact a))
+compactNew (W# size) val = do
+  -- cheap trick: () is a constant and so Compact () is the empty compact
+  -- and we don't need to append or adjust the address
+  unitStr <- IO (\s -> case compactNew# size s of
+                    (# s', buffer #) -> (# s', Compact buffer () #) )
+  compactAppendRecursively unitStr val
+
+compactAppendEvaled :: Compact b -> a -> IO (Maybe (Compact a))
+compactAppendEvaled str !root =
+  let buffer = compactGetBuffer str
+  in
+   IO (\s -> compactAppendEvaledInternal buffer root 0# s)
+
+class Compactable a where
+  compact :: Compact b -> a -> IO (Maybe (Compact a))
+
+compactAppendRecursively :: Compactable a => Compact b -> a -> IO (Maybe (Compact a))
+compactAppendRecursively str@(Compact buffer _) !val = do
+  if isTrue# (compactContains# buffer val) then
+    return $ Just $ Compact buffer val
+    else if isTrue# (compactContainsAny# val) then
+           compactAppendEvaled str val
+         else
+           compact str val
+
+compactAppendOne :: Compact b -> a -> IO (Maybe (Compact a))
+compactAppendOne (Compact buffer _) !val =
+  IO (\s -> case compactAppendOne# buffer val s of
+         (# s', rootAddr #) -> (# s', maybeMakeCompact buffer rootAddr #) )
+
+-- | 'defaultCompactNFData': a default implementation for compact suitable
+-- | for NFData instances
+defaultCompactNFData :: NFData a => Compact b -> a -> IO (Maybe (Compact a))
+defaultCompactNFData str v = compactAppendEvaled str (force v)
+
+appendRec :: Compactable a => Compact b -> a -> MaybeT IO a
+appendRec str val = do
+  str' <- MaybeT $ compactAppendRecursively str val
+  return $ compactGetRoot str'
+
+instance Compactable a => Compactable [a] where
+  compact str [] = compactAppendOne str []
+  compact str (x:xs) = runMaybeT $ do
+    xs' <- appendRec str xs
+    x' <- appendRec str x
+    MaybeT $ compactAppendOne str (x':xs')
+
+instance Compactable () where
+  compact str val = compactAppendOne str val
+
+instance (Compactable a, Compactable b) => Compactable (a,b) where
+  compact str (l, r) = runMaybeT $ do
+    l' <- appendRec str l
+    r' <- appendRec str r
+    MaybeT $ compactAppendOne str (l', r')
+
+instance (Compactable a, Compactable b, Compactable c) =>
+         Compactable (a,b,c) where
+  compact str (v1, v2, v3) = runMaybeT $ do
+    v1' <- appendRec str v1
+    v2' <- appendRec str v2
+    v3' <- appendRec str v3
+    MaybeT $ compactAppendOne str (v1',v2',v3')
+
+instance (Compactable a, Compactable b, Compactable c) =>
+         Compactable (a,b,c,d) where
+  compact str (v1, v2, v3, v4) = runMaybeT $ do
+    v1' <- appendRec str v1
+    v2' <- appendRec str v2
+    v3' <- appendRec str v3
+    v4' <- appendRec str v4
+    MaybeT $ compactAppendOne str (v1',v2',v3',v4')
+
+instance (Compactable a, Compactable b, Compactable c, Compactable d,
+          Compactable e) => Compactable (a,b,c,d,e) where
+  compact str (v1, v2, v3) = runMaybeT $ do
+    v1' <- appendRec str v1
+    v2' <- appendRec str v2
+    v3' <- appendRec str v3
+    v4' <- appendRec str v4
+    v5' <- appendRec str v5
+    MaybeT $ compactAppendOne str (v1',v2',v3',v4',v5')
+
+instance Compactable a => Compactable (Maybe a) where
+  compact str Nothing = compactAppendOne str Nothing
+  compact str (Just v) = runMaybeT $ do
+    v' <- appendRec str v
+    MaybeT $ compactAppendOne str (Just v')
+
+instance (Compactable a, Compactable b) => Compactable (Either a b) where
+  compact str (Left l) = runMaybeT $ do
+    l' <- appendRec str l
+    MaybeT $ compactAppendOne str (Left l')
+  compact str (Right r) = runMaybeT $ do
+    r' <- appendRec str r
+    MaybeT $ compactAppendOne str (Right r')
+
+instance Compactable Int where
+  compact str v = compactAppendOne str v
+
+instance Compactable Char where
+  compact str v = compactAppendOne str v
+
+instance Compactable Float where
+  compact str v = compactAppendOne str v
+
+instance Compactable Word where
+  compact str v = compactAppendOne str v
+
+instance Compactable Integer where
+  compact = defaultCompactNFData
+
+instance Compactable Double where
+  compact = defaultCompactNFData
---
+++ libraries/compact/Data/Compact/Monad.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Compact.Monad
+-- Copyright   :  (c) The University of Glasgow 2001-2009
+--                (c) Giovanni Campagna <gcampagn@cs.stanford.edu> 2015
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  unstable
+-- Portability :  non-portable (GHC Extensions)
+--
+-- This module provides a data structure, called a Compact, for holding
+-- a set of fully evaluated Haskell values in a consecutive block of
+-- memory.
+--
+-- As the data fully evaluated and pure (thus immutable), it maintains
+-- the invariant that no memory reference exist from objects inside the
+-- Compact to objects outside, thus allowing very fast garbage collection
+-- (at the expense of increased memory usage, because the entire set of
+-- object is kept alive if any object is alive).
+--
+-- This module provides a Monad for executing code in the context of a
+-- compact, so that all values threaded through the monad are stored in
+-- the compact.
+--
+-- /Since: 1.0.0/
+
+module Data.Compact.Monad (
+  CompactM,
+  runCompactM,
+  compactPut,
+  ) where
+
+import Control.Monad
+import Control.Applicative
+import Data.Compact.Incremental
+
+newtype CompactM b a = CompactM {
+  runCompactM :: Compact b -> IO (Maybe (Compact a)) }
+
+instance Monad (CompactM b) where
+  return v = CompactM $ \str -> compactAppendOne str v
+
+  m >>= f = CompactM $ \str -> do
+    mstr' <- runCompactM m str
+    case mstr' of
+      Nothing -> return Nothing
+      Just str' -> let v = compactGetRoot str'
+                       -- you may think that this is str', because
+                       -- CompactM behaves like a state monad, where
+                       -- the compact is threaded down
+                       -- but if you do so you get type errors because
+                       -- runCompactM here wants a Compact b, where b
+                       -- is rigidly bound by the instance declaration,
+                       -- while str' is a Compact a, where a is rigidly
+                       -- bound by the signature of >>=
+                       -- (this is why state monads don't let you change
+                       -- the type of the state!)
+                       -- but in this specific case it doesn't matter,
+                       -- because both str and str' share the underlying
+                       -- buffer, which is what we need to thread the
+                       -- state down for
+                   in v `seq` runCompactM (f v) str
+
+instance Applicative (CompactM b) where
+  pure = return
+  (<*>) = ap
+
+instance Functor (CompactM b) where
+  fmap = liftM
+
+compactPut :: Compactable a => a -> CompactM b a
+compactPut v = CompactM $ \str -> compactAppendRecursively str v
+
