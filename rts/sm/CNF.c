a5c20b97be311347b1a39a5ee12400d436292b2f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-02-11 13:28:50 -0800

    CNF: fix copy-paste error

--- rts/sm/CNF.c
+++ rts/sm/CNF.c
@@ -289,9 +289,9 @@ compactAllocateBlockAtInternal (Capability *cap,
     // Sanity check the address before making bogus calls to the
     // block allocator
     if (can_alloc_group_at(addr, aligned_size))
-        return compactAllocateBlock(cap, aligned_size, addr, NULL, rtsFalse);
+        return compactAllocateBlock(cap, aligned_size, addr, NULL, linkGeneration);
     else
-        return compactAllocateBlock(cap, aligned_size, NULL, NULL, rtsFalse);
+        return compactAllocateBlock(cap, aligned_size, NULL, NULL, linkGeneration);
 }
 
 #else
@@ -302,7 +302,7 @@ compactAllocateBlockAtInternal (Capability *cap,
                                 StgWord     aligned_size,
                                 rtsBool     linkGeneration)
 {
-    return compactAllocateBlock(cap, aligned_size, NULL, NULL, rtsFalse);
+    return compactAllocateBlock(cap, aligned_size, NULL, NULL, linkGeneration);
 }
 
 #endif // USE_STRIPED_ALLOCATOR
