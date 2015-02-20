383c5cbdeefbe2eaa4e47298eedf73f8a8f755de
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-02-19 19:08:04 -0800

    CNF: have compacts count toward minor GCs
    Otherwise loops that compactAppend cause an explosion in memory
    usage.

--- rts/sm/Storage.c
+++ rts/sm/Storage.c
@@ -1227,7 +1227,8 @@ calcNeeded (rtsBool force_major, memcount *blocks_needed)
         gen = &generations[g];
 
         blocks = gen->n_blocks // or: gen->n_words / BLOCK_SIZE_W (?)
-               + gen->n_large_blocks;
+               + gen->n_large_blocks
+               + gen->n_compact_blocks;
 
         // we need at least this much space
         needed += blocks;
