e42f7632f7e8c24e774d4f7fc4f1abebf26e11a8
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-02-21 18:28:16 -0800

    GC: fix memory accounting for compacts
    Otherwise we trigger major GCs too often

--- rts/sm/GC.c
+++ rts/sm/GC.c
@@ -1520,7 +1520,8 @@ resize_generations (void)
             words = oldest_gen->n_words;
         }
         live = (words + BLOCK_SIZE_W - 1) / BLOCK_SIZE_W +
-            oldest_gen->n_large_blocks;
+            oldest_gen->n_large_blocks +
+            oldest_gen->n_compact_blocks;
 
         // default max size for all generations except zero
         size = stg_max(live * RtsFlags.GcFlags.oldGenFactor,
