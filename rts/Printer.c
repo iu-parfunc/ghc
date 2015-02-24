d5e5d38a9854657efdb540f9434ca3b8ffc42036
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-02-23 18:49:31 -0800

    CNF: fix heap profiling compacts
    If we don't give StgCompactNFData a type name, it will be ignored
    by the heap profiler.

--- rts/Printer.c
+++ rts/Printer.c
@@ -952,7 +952,8 @@ char *closure_type_names[] = {
  [ATOMICALLY_FRAME]      = "ATOMICALLY_FRAME",
  [CATCH_RETRY_FRAME]     = "CATCH_RETRY_FRAME",
  [CATCH_STM_FRAME]       = "CATCH_STM_FRAME",
- [WHITEHOLE]             = "WHITEHOLE"
+ [WHITEHOLE]             = "WHITEHOLE",
+ [COMPACT_NFDATA]        = "COMPACT_NFDATA"
 };
 
 char *
