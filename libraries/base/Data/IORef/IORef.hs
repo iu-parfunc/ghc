fad34426a58ca9584b093cdae323c518584e21f2
Mike Vollmer <vollmerm@indiana.edu>
2016-05-10 17:15:04 -0400

    Update IORef in base, always use atomic operations

---
+++ libraries/base/Data/IORef/IORef.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE Trustworthy #-} -- is it?
+{-# LANGUAGE CPP, NoImplicitPrelude #-}
+
+module Data.IORef.IORef
+    (
+        -- * IORefs
+        IORef,                -- abstract, instance of: Eq, Typeable
+        newIORef,
+        readIORef,
+        writeIORef,
+        modifyIORef,
+        modifyIORef',
+        atomicModifyIORef,
+        atomicModifyIORef',
+        atomicWriteIORef,
+
+#if !defined(__PARALLEL_HASKELL__)
+        mkWeakIORef,
+#endif
+        ) where
+
+
+import GHC.Base
+-- import GHC.STRef
+-- import GHC.IORef hiding (atomicModifyIORef)
+-- import qualified GHC.IORef
+#if !defined(__PARALLEL_HASKELL__)
+import GHC.Weak
+#endif
+import Data.IORef.Unsafe (IORef(..), newIORef, readIORef,
+                          atomicModifyIORef, atomicModifyIORef',
+                          atomicWriteIORef)
+import qualified Data.IORef.Unsafe as U
+
+
+-- Are we going to provide alternate, safer implementations of these?
+modifyIORef :: IORef a -> (a -> a) -> IO ()
+modifyIORef a f = do
+  atomicModifyIORef a (\x -> (f x, ()))
+  return ()
+-- modifyIORef = U.modifyIORef
+modifyIORef' :: IORef a -> (a -> a) -> IO ()
+modifyIORef' a f = do
+  atomicModifyIORef' a (\x -> (f x, ()))
+  return ()                
+-- modifyIORef' = U.modifyIORef'
+writeIORef  :: IORef a -> a -> IO ()
+writeIORef = U.atomicWriteIORef
+{-# INLINE modifyIORef #-}
+{-# INLINE modifyIORef' #-}
+{-# INLINE writeIORef #-}
+                   
+#if !defined(__PARALLEL_HASKELL__)
+mkWeakIORef :: IORef a -> IO () -> IO (Weak (IORef a))
+mkWeakIORef = U.mkWeakIORef
+#endif
+
