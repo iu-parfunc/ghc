532cf6a55450e68a5e1e2cfd084d40b48e376dce
Parfunc Account <parfunc@googlegroups.com>
2016-07-13 00:10:46 -0400

    Enable counting storeLoadBarrier events for debugging and sanity checks
    Conflicts:
    	libraries/base/GHC/Barrier.hs

--- libraries/base/GHC/Barrier.hs
+++ libraries/base/GHC/Barrier.hs
@@ -9,7 +9,20 @@ module GHC.Barrier (
    ) where
 
 import GHC.Base
+import GHC.IORef
 import GHC.IO
+import System.IO.Unsafe (unsafePerformIO)
+import GHC.Word
+-- import Prelude ((+),(++),show)
+import GHC.Show (show)
+import GHC.Num ((+))
+import GHC.Foreign (withCString)
+import GHC.IO.Encoding (utf8)
+import GHC.Ptr (Ptr(..))
+
+{-# NOINLINE barrierCounts #-}
+barrierCounts :: IORef Word64
+barrierCounts = unsafePerformIO (newIORef 0)
 
 -- | Issue a Store/Load barrier.  Implementation varies by computing architecture.
 --   The effect is to prevent earlier store operations from being
@@ -20,3 +33,9 @@ storeLoadBarrier =
      IO $ \s1# ->
        case storeLoadBarrier# s1# of
          s2# -> (# s2#, () #)
+
+-- Duplicated from Debug.Trace:
+traceEventIO :: String -> IO ()
+traceEventIO msg =
+    withCString utf8 msg $ \(Ptr p) -> IO $ \s ->
+      case traceEvent# p s of s' -> (# s', () #)
