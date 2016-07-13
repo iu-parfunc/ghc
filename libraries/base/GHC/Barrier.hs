3f2f9318c523840b282088ba4dcbc68b531b1eb1
Parfunc Account <parfunc@googlegroups.com>
2016-07-13 10:31:39 -0400

    One more fix to break cycles

--- libraries/base/GHC/Barrier.hs
+++ libraries/base/GHC/Barrier.hs
@@ -13,11 +13,10 @@ import GHC.IORef
 import GHC.IO
 import System.IO.Unsafe (unsafePerformIO)
 import GHC.Word
--- import Prelude ((+),(++),show)
 import GHC.Show (show)
 import GHC.Num ((+))
 import GHC.Foreign (withCString)
-import GHC.IO.Encoding (utf8)
+import GHC.IO.Encoding.UTF8 (utf8)
 import GHC.Ptr (Ptr(..))
 
 {-# NOINLINE barrierCounts #-}
@@ -30,11 +29,13 @@ barrierCounts = unsafePerformIO (newIORef 0)
 {-# INLINE storeLoadBarrier #-}
 storeLoadBarrier :: IO ()
 storeLoadBarrier =
+  do n <- atomicModifyIORef barrierCounts (\n -> (n+1,n+1))
+     traceEventIO ("StoreLoadBarriers: "++show n)
      IO $ \s1# ->
        case storeLoadBarrier# s1# of
          s2# -> (# s2#, () #)
 
--- Duplicated from Debug.Trace:
+-- Duplicated from Debug.Trace to avoid cycles:
 traceEventIO :: String -> IO ()
 traceEventIO msg =
     withCString utf8 msg $ \(Ptr p) -> IO $ \s ->
